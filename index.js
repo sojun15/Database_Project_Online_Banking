3d8684b809f5eb496912684cdac74b664af6f879
sojun15 <sojun2115@cseku.ac.bd>
2025-03-07 16:54:51 +0600

    today i will make login check condition and after login homepage visible

--- index.js
+++ index.js
@@ -12,24 +12,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/login.html');
+});
+
+app.get('/signup',(req,res)=>{
     res.sendFile(__dirname+'/signup.html');
 });
 
-app.post('/',(req,res)=>{
+app.post('/signup',(req,res)=>{
     // here disstructuring name and html form name must be same otherwise error
-    let {nid_number,name,phone,date_birth} = req.body;
+    let {nid_number,name,phone,date_birth,password} = req.body;
 
     connect.connect((error)=>{
         if(error) throw error;
 
-        let sql = "insert into online_bank(Nid,Name,Phone,Date_birth) values(?,?,?,?)";
-        connect.query(sql,[nid_number,name,phone,date_birth],(error,result)=>{
+        let sql = "insert into online_bank(Nid,Name,Phone,Date_birth,Password) values(?,?,?,?,?)";
+        connect.query(sql,[nid_number,name,phone,date_birth,password],(error,result)=>{
             if(error) throw error;
             // after passing query we must be send any message as response otherwise there will be error
             res.send('data insert successfully');
         })
     })
 });
+
+app.post('/', (req, res) => {
+    let { nid_number, password } = req.body; // Destructure nid and password from form
+
+    connect.connect((error) => {
+        if (error) throw error;
+
+        let sql = "SELECT * FROM online_bank WHERE Nid = ? AND Password = ?";
+        connect.query(sql, [nid_number, password], (err, result) => {
+            if (err) throw err;
+
+            if (result.length > 0) {
+                res.redirect('/homepage.html');
+            } else {
+                res.send('Invalid Nid or Password ❌');
+            }
+        });
+    });
+});
+
 
 app.listen(5500,()=>{
     console.log('server is running on: http://localhost:5500');
