7c482cd8f5e2566b8dbd9b96d4be30f7b82b865d
sojun15 <sojun2115@cseku.ac.bd>
2025-05-11 16:04:34 +0600

    when user send money or cashout then check both phone number and password are valid or not then transection performed

--- index.js
+++ index.js
@@ -49,11 +49,11 @@ app.post('/send_money',(req,res,next)=>{
     // if user login then there have some value into sender_phone
     if(!sender_phone) return res.status(401).send('session time out, please login again');
 
-    let sending_amount = "select Money from online_bank where Phone = ?";
+    let sending_amount = "select Money from online_bank where Phone = ? and Password = ?";
     // if sender_phone find into database then it keep some value into result
-    connect.query(sending_amount,[sender_phone],(error,result)=>{
+    connect.query(sending_amount,[sender_phone,pin_code],(error,result)=>{
         if(error) return next(error);
-        if(result.length===0) return res.status(401).send('sender not found');
+        if(result.length===0) return res.status(401).send('Invalid number or password');
         
         const sender_balance = parseFloat(result[0].Money);
         // if sender have insufficient balance then it will back from here
@@ -64,7 +64,7 @@ app.post('/send_money',(req,res,next)=>{
 
         // transection transfering process starts here
         connect.beginTransaction(error =>{
-            if(error) throw next(error);
+            if(error) return next(error);
     
             // sender amount decrease segment
             const decrease_amount = "update online_bank set Money = Money - ? where Phone = ?";
@@ -94,14 +94,49 @@ app.post('/send_money',(req,res,next)=>{
 })
 
 app.post('/cashout',(req,res,next)=>{
-    let {phone,amount,pin_code} = req.body;
+    let {phone:agent_phone,amount,pin_code} = req.body;
+    let sender_phone = req.session.phone;
+    let transfer_amount = parseFloat(amount);
 
-    let sql = "insert into cashout(Phone,Amount) values(?,?)";
-    connect.query(sql,[phone,amount],(error,result)=>{
-        if(error) throw next(error);
-        res.redirect('/completed.html');
-    })
-})
+    if(!sender_phone) return res.status(401).send('login expired, please login again');
+
+    let cashout_money = "select Money from online_bank where Phone = ? and Password = ?";
+    connect.query(cashout_money,[sender_phone,pin_code],(error,result)=>{
+        if(error) return res.status(401).send('Invalid phone or password')
+
+        if(result.length===0) return res.status(401).send('Invalid phone or password');
+
+        let sender_balance = parseFloat(result[0].Money);
+
+        if(sender_balance<transfer_amount)
+            return res.status(401).send('insufficient balance');
+
+        connect.beginTransaction(error=>{
+            if(error) return next(error);
+
+            let decrease_amount = "update online_bank set Money = Money - ? where Phone = ?";
+            connect.query(decrease_amount,[transfer_amount,sender_phone],(error)=>{
+                if(error) return connect.rollback(()=> next(error));
+
+                let increase_amount = "update online_bank set Money = Money + ? where Phone = ?";
+                connect.query(increase_amount,[transfer_amount,agent_phone],(error)=>{
+                    if(error) return connect.rollback(()=> next(error));
+
+                    let cashout_table = "insert into cashout(Phone,Amount) values(?,?)";
+                    connect.query(cashout_table,[agent_phone,transfer_amount],(error)=>{
+                        if(error) return connect.rollback(()=>next(error));
+
+                        connect.commit((error)=>{
+                            if(error) return connect.rollback(()=> next(error));
+
+                            res.redirect('completed.html');
+                        })
+                    });
+                });
+            });
+        });
+    });
+});
 
 app.post('/recharge',(req,res,next)=>{
     let {mobile,amount,pin_code} = req.body;
